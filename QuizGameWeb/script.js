d81bfd02bc03ed1fdc2179db76243c3d2a59b00f
EvgOleK <Keocool2007@yandex.ru>
2025-12-17 10:09:21 +0300

    Add files via upload

---
+++ QuizGameWeb/script.js
@@ -0,0 +1,68 @@
+import { Questions } from "./question.js";
+import { arrayQuiz } from "./question.js";
+
+const startBut = document.getElementById('start');
+const backBut = document.getElementById("back");
+const answersBut = Array.from(document.getElementsByClassName("answer"));
+
+const firstBlock = document.getElementById('firstBlock');
+const secondBlock = document.getElementById('secondBlock');
+const thirdBlock = document.getElementById('thirdBlock');
+
+const questionText = document.getElementById('question');
+const scoreText = document.getElementById("score");
+let score = 0;
+
+let arrayObject = [];
+for(let i = 0; i < arrayQuiz.length; ++i){
+    arrayObject[i] = new Questions(arrayQuiz[i].question, arrayQuiz[i].answerText, arrayQuiz[i].correct);
+}
+
+function quest(j){
+    questionText.textContent = arrayObject[j].questions;
+    for(let i = 0; i < answersBut.length; ++i){
+        answersBut[i].textContent = arrayObject[j].answer[i];
+        answersBut[i].onclick = function(){
+            if(arrayObject[j].correct === i){
+                console.log('good');
+                score += 1;
+            } else{
+                console.log("not good");
+            }
+
+            if(j < arrayObject.length - 1){
+                quest(++j);
+            } else{
+                endQuiz();
+            }
+        }
+    }  
+}
+
+function startQuiz(){
+    secondBlock.style.display = 'block';
+    firstBlock.style.display = 'none';
+    quest(0);
+}
+
+function endQuiz(){
+    if(score == 10){
+        scoreText.textContent = `Поздравляю! Ваш счёт: ${score}`;
+        secondBlock.style.display = 'none';
+        thirdBlock.style.display = 'flex';
+    }
+    else{
+        scoreText.textContent = `Ваш счёт: ${score}`;
+        secondBlock.style.display = 'none';
+        thirdBlock.style.display = 'flex';
+    }
+}
+
+function backMain(){
+    firstBlock.style.display = 'flex';
+    thirdBlock.style.display = 'none';
+    score = 0;
+}
+
+startBut.addEventListener('click', startQuiz);
+backBut.addEventListener('click', backMain);
